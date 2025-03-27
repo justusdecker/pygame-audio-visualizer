d3159101822ee224bcd5126579cc7d3bd821b497
justusdecker <justus.d2025@gmail.com>
2025-03-27 01:57:19 +0100

    0.2 - Object rendering

--- main.py
+++ main.py
@@ -2,17 +2,40 @@
 import pygame as pg
 from time import perf_counter
 from tkinter.filedialog import askopenfilename,asksaveasfilename
+from os import path
 import wave
 BACKGROUND_COLOR = (36,36,36)
+def get_image(title:str) -> pg.Surface:
+    filepath = askopenfilename(title=title,filetypes=[("Images",(".png",".jpeg",".jpg",".webp"))])
+    if path.isfile(filepath):
+        return pg.image.load(filepath)
+    return pg.Surface((1,1),pg.SRCALPHA)
 
 class Animator:
     def __init__(self):
-        self.foreground_image = pg.image.load
-        self.background_image = pg.image.load
+        self.background_image = get_image("Background image")
+        self.foreground_image = get_image("Foreground image")
+        
+        
+        self.bi_scale = 1.
+        self.fi_scale = 1.
+        
+        self.fi_pos = [640-(self.foreground_image.get_width()//2),360-(self.foreground_image.get_height()//2)]
+        self.bi_pos = [0,0]
+        
+        self.bi_rot = 0
+        self.fi_rot = 0
+        
+        self.fi_pivot = self.fi_pos.copy()
+        self.bi_pivot = self.bi_pos.copy()
+        
         self.particle_effects = None
         self.particle_animation = None
         self.music = None
         self.destination = None
+    def show(self,app):
+        app.window.blit(self.background_image,self.bi_pos)
+        app.window.blit(self.foreground_image,self.fi_pos)
     def render(self):
         pass
 
@@ -24,7 +47,7 @@ class App:
     window = pg.display.set_mode((width,height))
     is_running = True
     def __init__(self):
-        pass
+        self.animator = Animator()
     def run(self):
         while self.is_running:
             start_time = perf_counter()
@@ -33,6 +56,7 @@ class App:
             self.check_events()
             self.delta_time = perf_counter() - start_time
     def update(self):
+        self.animator.show(self)
         pg.display.update()
     def check_events(self):
         for event in pg.event.get():
